84240dc0b4cf528dee35b72b3449f0a6dd83b919
SihaO <SihaO@SihaOs-MacBook-Air.local>
2015-09-23 18:23:03 +0800

    Patched Bug: Change IP Address Limit from String to Numeric

--- tools/constants/limits.php
+++ tools/constants/limits.php
@@ -16,7 +16,7 @@ class Limits {
 	private static $password_limit = array('min' => 7, 'max' => 30);
 	private static $password_hash_limit = array('min' => 32, 'max' => 32);
 	private static $email_limit = array('min' => 3, 'max' => 320);
-	private static $ip_address_limit = array('min' => '7', 'max' => 45);
+	private static $ip_address_limit = array('min' => 7, 'max' => 45);
 	
 	//access method
 	public static function get($key) { return self::$$key; }
